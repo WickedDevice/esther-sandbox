a130e9cc8e120b739d43aed19fc27ff6a90c4f0a
swsheep <wong.369@osu.edu>
2014-07-17 19:14:54 -0400

    Library for LMP91000

---
+++ LMP91000/lmp91000.cpp
@@ -0,0 +1,68 @@
+#include "LMP91000.h"
+
+LMP91000::LMP91000(uint8_t menb_pin, uint8_t tiacn, uint8_t refcn, uint8_t modecn) {
+	_menb = menb_pin;
+	_tiacn = tiacn;
+	_refcn = refcn;
+	_modecn = modecn;
+}
+
+//assumes menb is handled correctly
+void LMP91000::write(uint8_t reg, uint8_t data) {
+	Wire.beginTransmission(LMP91000_I2C_Address_W);
+	Wire.write(reg);
+	Wire.write(data);
+	Wire.endTransmission();
+}
+
+uint8_t LMP91000::status(void) {
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	Wire.write(LMP91000_STATUS_REG);
+	if (Wire.endTransmission() != 0) {
+		return LMP91000_NOT_PRESENT;
+	}
+	uint8_t ready = 0;
+	Wire.requestFrom(LMP91000_I2C_Address, 1);
+	if(Wire.available()) {
+		ready = Wire.read();
+	}
+	return ready &= 1;
+}	
+	
+	//move enable handling to main, out of library
+bool LMP91000::begin() {		
+	Wire.begin();
+	//check if ready
+	//uint8_t ready = 0;
+	while (status() != LMP91000_READY) {
+		if (status() == LMP91000_NOT_PRESENT) {
+			return false;
+		}
+	}
+	
+	//set TIA reg
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	write(LMP91000_LOCK_REG, LMP91000_WRITE_UNLOCK);
+	Wire.endTransmission();
+	
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	write(LMP91000_TIACN_REG, _tiacn);
+	Wire.endTransmission();
+	
+	//set REFCN reg
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	write(LMP91000_REFCN_REG, _refcn);
+	Wire.endTransmission();
+	
+	//set MODECN reg
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	write(LMP91000_MODECN_REG, _modecn);
+	Wire.endTransmission();
+	
+	Wire.beginTransmission(LMP91000_I2C_Address);
+	write(LMP91000_LOCK_REG, LMP91000_WRITE_LOCK);
+	Wire.endTransmission();
+	
+	return true;
+}
+
---
+++ LMP91000/lmp91000.h
@@ -0,0 +1,42 @@
+#ifndef LMP91000_H
+#define LMP91000_H
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include <Arduino.h>
+#else
+#include <WProgram.h>
+#endif
+
+#include <Wire.h>
+
+#define LMP91000_I2C_Address	0x48
+#define LMP91000_I2C_Address_R	(0x91)                  // Device Address
+#define LMP91000_I2C_Address_W	(0x90) 
+#define LMP91000_STATUS_REG	(0x00)                  /* Read only status register */
+#define LMP91000_LOCK_REG	(0x01)                  /* Protection Register */
+#define LMP91000_TIACN_REG	(0x10)                  /* TIA Control Register */
+#define LMP91000_REFCN_REG	(0x11)                  /* Reference Control Register*/
+#define LMP91000_MODECN_REG	(0x12)                  /* Mode Control Register */
+
+#define LMP91000_WRITE_LOCK	(0x01)
+#define LMP91000_WRITE_UNLOCK	(0x00)
+#define LMP91000_READY	(0x01)
+#define LMP91000_NOT_READY	(0x00)
+#define LMP91000_NOT_PRESENT 0xA8
+
+class LMP91000 {
+
+public:
+	LMP91000(uint8_t _menb, uint8_t tiacn, uint8_t refcn, uint8_t modecn);
+	bool begin(void);
+	void write(uint8_t reg, uint8_t data);
+	uint8_t status(void);
+	
+private:
+	uint8_t _menb;
+	uint8_t _tiacn;
+	uint8_t _refcn;
+	uint8_t _modecn;
+};
+
+#endif
